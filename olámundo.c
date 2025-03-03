1885877000d54122700a27764f94dc1a4101ec6b
programandoemc <ceroberio@hotmail.com>
2025-03-03 02:57:43 +0000

    Super Trunfo3

--- olámundo.c
+++ olámundo.c
@@ -3,16 +3,20 @@
 int main () {
     char Carta1 [6];
     char Estado [5]= "Ceara";
+    char Codigo[3]= "A01";
     char NomedaCidade [12]= "Fortaleza";
-    int Populacao="1431658500";
-    int Area="1431658511 ";
-    int NumerosdePontosTuristicos= "50";
+    int Populacao=2000000;
+    int Area=10000000;
+    float Pib= 699.28;
+    int NumerosdePontosTuristicos= 50;
 
      printf("Carta1: %s \n",Carta1); 
      printf("Estado: %s \n", Estado);
+     printf("Codigo: %s \n", Codigo);
      printf("Nome da Cidade: %s \n", NomedaCidade);
      printf("Populacao: %d \n", Populacao);
      printf("Area: %d \n", Area);
+     printf("Pib : %2f \n", Pib);
      printf("Numeros de Pontos Turisticos: %d \n",NumerosdePontosTuristicos);
 
     return 0;
