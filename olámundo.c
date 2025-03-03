ef4956f22f83bc924b07f0c2786ebd8778557300
programandoemc <ceroberio@hotmail.com>
2025-03-03 01:29:48 +0000

    Super Trunfo

--- olámundo.c
+++ olámundo.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main () {
+    char Carta1 [6];
+    char Estado [5]= "Ceara";
+    char NomedaCidade [12]= "Fortaleza";
+    int população="12325000";
+
+     printf("Carta1 %s \n",Carta1); 
+     printf("Estado: %s \n", Estado);
+     printf("Nome da Cidade: %s \n", NomedaCidade);
+     printf("população: %d \n", população);
+
+
+    return 0;
+}
